3b8eed3432bf86fcf04d125e6e7587fed9256481
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-07-16 14:50:47 +0100

    first commit

---
+++ activesite/activityapp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import  Visit, Team,  UserTeam, Audit, Task, Profile
+
+class visitAdmin(admin.ModelAdmin):
+    list_display = ["user", "date","goal","achievement","notes","picture","created_at","updated_at"]
+
+#admin.site.register(User)
+admin.site.register(Visit, visitAdmin)
+#admin.site.register(Visit)
+admin.site.register(Team)
+admin.site.register(UserTeam)
+admin.site.register(Audit)
+admin.site.register(Profile)
+admin.site.register(Task)
---
+++ activesite/activityapp/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from PIL import Image as Im 
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+class Task(models.Model):
+    task = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    region = models.CharField(max_length=100)
+    area = models.CharField(max_length=100)
+    completed = models.CharField(max_length=100)
+   
+
+class Visit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    mission = models.ForeignKey(Task, on_delete=models.CASCADE)
+    goal = models.IntegerField()
+    achievement = models.IntegerField()
+    notes = models.TextField()
+    picture = models.ImageField(upload_to='pics')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+    
+
+class UserTeam(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    month = models.DateField()
+    goal = models.IntegerField()
+    achievement = models.IntegerField()
+   
+
+class Audit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    findings = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
