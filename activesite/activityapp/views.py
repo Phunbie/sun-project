de71c02c99c6204231a076d328a28156d041159e
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-07-18 18:29:16 +0100

    start adding views

--- activesite/activityapp/views.py
+++ activesite/activityapp/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .models import Task, Profile, Team
+from .models import Profile, Team
 
 
 def index(request):
-    #return render(request, "account/base.html")
-    return HttpResponse("Hello, world. You're at the polls index.")
+    #task = Task.objects.all()
+    task = Task.objects.filter(approve=False, completed=False)
+    completed = Task.objects.filter(approve=False, completed=True)
+    approve = Task.objects.filter(approve=True, completed=True)
+    tas = {'task':task,'completed':completed,'approve':approve}
+    return render(request, "index.html", tas)
+    #return HttpResponse("Hello, world. You're at the polls index.")
+
+def dashboard(request):
+    #task = Task.objects.all()
+    task = Task.objects.filter(approve=False, completed=False)
+    completed = Task.objects.filter(approve=False, completed=True)
+    approve = Task.objects.filter(approve=True, completed=True)
+    tas = {'task':task,'completed':completed,'approve':approve}
+    return render(request, "account/index.html", tas)
+    
+def team_profiles(request, id):
+    team = get_object_or_404(Team, id=id)
+    profiles = Profile.objects.filter(team=team)
+    return render(request, 'team_profiles.html', {'profiles': profiles})
