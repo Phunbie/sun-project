2287ca6408513f9ca94f3173a36b83d85a6555fd
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-07-26 18:01:06 +0100

    allowed local

--- activesite/activesite/settings.py
+++ activesite/activesite/settings.py
@@ -29,7 +29,7 @@ goog = os.environ.get('secgoo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['phunbie.pythonanywhere.com']
+ALLOWED_HOSTS = ['*','127.0.0.1:8000']
 
 
 # Application definition
